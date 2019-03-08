e140c09fc8addca8c5a54526e37e7dfef509843e
Denis Peshkov (SmartGit) <peshmaxan@gmail.com>
2019-03-08 16:41:06 +0300

    Changed jackson version

--- src/main/java/ru/eltech/dapeshkov/speed_layer/Main.java
+++ src/main/java/ru/eltech/dapeshkov/speed_layer/Main.java
@@ -1,11 +1,16 @@
 package ru.eltech.dapeshkov.speed_layer;
 
+import ru.eltech.spark.Batch;
+import ru.eltech.spark.Streaming;
+
 /**
  * This is a test class
  */
 
 public class Main {
     public static void main(final String[] args) {
+        Batch.start();
+        Streaming.start();
         final NewsReader reader = new NewsReader("files/", "Google", "Google");
         reader.start();
     }
