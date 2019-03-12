6c5f5dcc6fc6ef3868a39745be2cc779ef8be6e0
Denis Peshkov (SourceTree) <peshmaxan@gmail.com>
2019-03-12 16:33:05 +0300

    Add modules

---
+++ Batch/src/ru/eltech/spark/Batch.java
@@ -0,0 +1,36 @@
+package ru.eltech.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class Batch {
+
+    private Batch() {
+
+    }
+
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
+        System.setProperty("hadoop.home.dir", "C:\\winutils\\");
+        SparkConf conf = new SparkConf().setAppName("Batch");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        while (true) {
+            JavaRDD<String> distFile = sc.textFile("files");
+            if (!distFile.isEmpty())
+                distFile.foreach(s -> System.out.println(s));
+            else {
+                System.out.println("empty");
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
