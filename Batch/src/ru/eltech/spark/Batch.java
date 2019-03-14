1977e8989c23e6e1efef4bcb271e0e934ed2fb1e
Denis Peshkov (SourceTree) <peshmaxan@gmail.com>
2019-03-14 23:03:58 +0300

    Spark

--- Batch/src/ru/eltech/spark/Batch.java
+++ Batch/src/ru/eltech/spark/Batch.java
@@ -3,6 +3,11 @@ package ru.eltech.spark;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.io.Serializable;
 
 public class Batch {
 
@@ -16,21 +21,32 @@ public class Batch {
 
     public static void start() {
         System.setProperty("hadoop.home.dir", "C:\\winutils\\");
-        SparkConf conf = new SparkConf().setAppName("Batch");
+        SparkConf conf = new SparkConf().setAppName("Batch").setMaster("local[2]");
 
         JavaSparkContext sc = new JavaSparkContext(conf);
         while (true) {
-            JavaRDD<String> distFile = sc.textFile("files");
-            if (!distFile.isEmpty())
-                distFile.foreach(s -> System.out.println(s));
-            else {
-                System.out.println("empty");
-            }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            JavaRDD<String> distFile = sc.textFile("Z:\\bachelor-diploma\\files");
+            JavaRDD<Int> lengths = distFile.map(s -> new Int(s.length()));
+            SparkSession session = SparkSession.builder().sparkContext(lengths.context()).getOrCreate();
+            Dataset<Row> dataset = session.createDataFrame(lengths, Int.class);
+            dataset.show();
+            dataset.write().json("./asasas/");
+        }
+    }
+
+    public static class Int implements Serializable {
+        int value;
+
+        public Int(int value) {
+            this.value = value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
         }
     }
 }
