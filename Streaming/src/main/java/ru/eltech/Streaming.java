7618788c81e62f319755c7083cd0864969a5e594
Denis Peshkov (SourceTree) <peshmaxan@gmail.com>
2019-04-01 22:12:40 +0300

    Watcher

--- Streaming/src/main/java/ru/eltech/Streaming.java
+++ Streaming/src/main/java/ru/eltech/Streaming.java
@@ -1,35 +1,64 @@
 package ru.eltech;
 
+import org.apache.hadoop.yarn.webapp.hamlet.Hamlet;
 import org.apache.spark.SparkConf;
+import org.apache.spark.ml.PipelineModel;
 import org.apache.spark.sql.*;
-import org.apache.spark.sql.streaming.StreamingQuery;
-import org.apache.spark.sql.streaming.StreamingQueryException;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.Metadata;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
+import ru.eltech.Singltones.ArrayBlockingQueueSinglton;
+import ru.eltech.Singltones.WatcherSingltone;
+import ru.eltech.mapeshkov.spark.PredictionUtils;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Streaming {
 
     public static void main(String[] args) {
-        start();
+        startRDD();
     }
 
-    public static void start() {
+    public static void startRDD() {
 
         System.setProperty("hadoop.home.dir", "C:\\winutils\\");
 
-        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+        SparkConf conf = new SparkConf().setMaster("local[1]").setAppName("NetworkWordCount");
         JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
 
         JavaDStream<String> stringJavaDStream = jssc.textFileStream("files/");
 
-        stringJavaDStream.foreachRDD((rdd) -> { //driver
+        stringJavaDStream.foreachRDD(rdd -> { //driver
             SparkSession spark = SparkSession.builder().config(rdd.context().getConf()).getOrCreate();
-            rdd.collect().forEach(System.out::println); //driver
+
+            rdd.foreach(str -> {
+                ArrayBlockingQueue<Schema> queue = ArrayBlockingQueueSinglton.getHelper();
+                String[] split = str.split(",");
+                Schema record = new Schema();
+                record.setCompany(split[0]);
+                record.setSentiment(split[1]);
+                record.setYear(Integer.valueOf(split[2]));
+                record.setMonth(Integer.valueOf(split[3]));
+                record.setDay(Integer.valueOf(split[4]));
+                record.setToday_stock(Double.valueOf(split[5]));
+
+                queue.put(record);
+
+                if (queue.size() == 5) {
+                    Watcher watcher = WatcherSingltone.getHelper();
+                    PipelineModel model = null;
+                    if (watcher.check()) {
+                        model = PipelineModel.load("model/model");
+                    }
+
+                    if (model != null) {
+                    }
+                    queue.take(); //TODO latest element
+                }
+            });
+
+            //Dataset<Row> dataFrame = spark.createDataFrame(rowRDD, Schema.class);
         });
 
         jssc.start();
@@ -40,13 +69,4 @@ public class Streaming {
             e.printStackTrace();
         }
     }
-
-    private static final StructType schema = new StructType(new StructField[]{
-            new StructField("company", DataTypes.StringType, false, Metadata.empty()),
-            new StructField("sentiment", DataTypes.StringType, false, Metadata.empty()),
-            new StructField("year", DataTypes.IntegerType, false, Metadata.empty()),
-            new StructField("month", DataTypes.IntegerType, false, Metadata.empty()),
-            new StructField("day", DataTypes.IntegerType, false, Metadata.empty()),
-            new StructField("today_stock", DataTypes.DoubleType, false, Metadata.empty()),
-    });
 }
---
+++ Streaming/src/main/java/ru/eltech/Watcher.java
@@ -0,0 +1,62 @@
+package ru.eltech;
+
+import org.apache.spark.ml.regression.LinearRegression;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Watcher {
+
+    final private WatchService watchService;
+    final private String file;
+
+    public Watcher(Path path) throws IOException {
+        watchService = FileSystems.getDefault().newWatchService();
+        path.getParent().register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+        file = path.getName(path.getNameCount() - 1).toString();
+    }
+
+    public void process(Action action) {
+        while (true) {
+            final WatchKey wk;
+            try {
+                wk = watchService.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
+            for (WatchEvent<?> event : wk.pollEvents()) {
+                final Path changed = (Path) event.context();
+                if (changed.endsWith(file)) {
+                    action.run();
+                }
+            }
+            // reset the key
+            boolean valid = wk.reset();
+            if (!valid) {
+                System.err.println("Key has been unregistered");
+            }
+        }
+    }
+
+    public boolean check() {
+        final WatchKey wk;
+
+        wk = watchService.poll();
+
+        for (WatchEvent<?> event : wk.pollEvents()) {
+            final Path changed = (Path) event.context();
+            if (changed.endsWith(file)) {
+                return true;
+            }
+        }
+        // reset the key
+        boolean valid = wk.reset();
+        if (!valid) {
+            System.err.println("Key has been unregistered");
+        }
+        return false;
+    }
+}
