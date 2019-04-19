42966eec982dd20905c8691ec83d662ac08453c9
Denis Peshkov (SourceTree) <peshmaxan@gmail.com>
2019-04-19 16:50:32 +0300

    Deleted singltones

---
+++ Streaming/src/main/java/ru/eltech/Model.java
@@ -0,0 +1,26 @@
+package ru.eltech;
+
+import org.apache.spark.ml.PipelineModel;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Model {
+    private PipelineModel model = null;
+    private Watcher watcher = null;
+    private String path;
+
+    public Model(String path) throws IOException {
+        watcher = new Watcher(Paths.get(path));
+        this.path = path;
+    }
+
+    public PipelineModel getModel() {
+        if (model == null || watcher.check()) {
+            model = PipelineModel.load(path);
+        }
+
+        return model;
+    }
+}
