fa55a1a74082d351f2f99aebf133d5acddd23959
Denis Peshkov (SourceTree) <peshmaxan@gmail.com>
2019-04-22 16:05:07 +0300

    PrintWriter

--- Maksim/Plot/src/main/java/ru/eltech/mapeshkov/Main.java
+++ Maksim/Plot/src/main/java/ru/eltech/mapeshkov/Main.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) throws Exception {
         PlotHelper plotHelper = new PlotHelper("prediction/pred.txt");
+        plotHelper.setMaxSeriesLength(20);
 
         for (; ; ) {
             TimeUnit.SECONDS.sleep(1);
--- Maksim/Plot/src/main/java/ru/eltech/mapeshkov/PlotHelper.java
+++ Maksim/Plot/src/main/java/ru/eltech/mapeshkov/PlotHelper.java
@@ -38,4 +38,8 @@ public class PlotHelper {
             }
         }
     }
+
+    public void setMaxSeriesLength(int length) {
+        plot.setMaxSeriesLength(length);
+    }
 }
